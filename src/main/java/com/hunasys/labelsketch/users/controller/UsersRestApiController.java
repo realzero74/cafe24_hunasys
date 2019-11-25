3f4cbfa63dc4b8e61e036feefe197c6d617a8b5b
jinyoung <jinyoung@JY-Z800-J48N2FC>
2019-11-26 02:28:19 +0900

    톰캣 실행시 오류 수정 jackson lib 수정

--- src/main/java/com/hunasys/labelsketch/users/controller/UsersRestApiController.java
+++ src/main/java/com/hunasys/labelsketch/users/controller/UsersRestApiController.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import com.hunasys.labelsketch.common.vo.UsersVo;
-import com.hunasys.labelsketch.orders.vo.OrdersVo;
-import com.hunasys.labelsketch.users.vo.UsersVo;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hunasys.labelsketch.orders.vo.OrdersVo;
+import com.hunasys.labelsketch.users.vo.UsersVo;
 
 @RestController
 public class UsersRestApiController {
