b0b1bd7b00da865bfeb6771654c5d6235bff7c10
jinyoung <jinyoung@JY-Z800-J48N2FC>
2019-11-26 03:24:19 +0900

    xml 기본 템플릿 소스 복붙

--- src/main/java/com/hunasys/labelsketch/common/vo/CodeVo.java
+++ src/main/java/com/hunasys/labelsketch/common/vo/CodeVo.java
@@ -2,29 +2,29 @@ package com.hunasys.labelsketch.common.vo;
 
 public class CodeVo {
 
-	private String  code;
+	private String  groupCd;
+	private String  groupNm;
 	private String  itemCd;
-	private String  codeNm;
 	private String  itemNm;
 	private Integer ordered;
 	
-	public String getCode() {
-		return code;
+	public String getGroupCd() {
+		return groupCd;
 	}
-	public void setCode(String code) {
-		this.code = code;
+	public void setGroupCd(String groupCd) {
+		this.groupCd = groupCd;
+	}
+	public String getGroupNm() {
+		return groupNm;
+	}
+	public void setGroupNm(String groupNm) {
+		this.groupNm = groupNm;
 	}
 	public String getItemCd() {
 		return itemCd;
 	}
 	public void setItemCd(String itemCd) {
 		this.itemCd = itemCd;
-	}
-	public String getCodeNm() {
-		return codeNm;
-	}
-	public void setCodeNm(String codeNm) {
-		this.codeNm = codeNm;
 	}
 	public String getItemNm() {
 		return itemNm;
@@ -42,12 +42,12 @@ public class CodeVo {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("CodeVo [code=");
-		builder.append(code);
+		builder.append("CodeVo [groupCd=");
+		builder.append(groupCd);
+		builder.append(", groupNm=");
+		builder.append(groupNm);
 		builder.append(", itemCd=");
 		builder.append(itemCd);
-		builder.append(", codeNm=");
-		builder.append(codeNm);
 		builder.append(", itemNm=");
 		builder.append(itemNm);
 		builder.append(", ordered=");
