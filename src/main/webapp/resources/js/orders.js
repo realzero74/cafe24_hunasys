511e7a2e68593ec6bdfe7f5ad4b338de96564522
jinyoung <jinyoung@JY-Z800-J48N2FC>
2019-12-22 11:28:34 +0900

    12-22작업분

--- src/main/webapp/resources/js/orders.js
+++ src/main/webapp/resources/js/orders.js
@@ -109,7 +109,6 @@ var OrderViewHandler = function(transaction){
 		initDatepicker : initDatepicker,
 		initCalendar : initCalendar,
 		orderFormPopup : orderFormPopup
-		
 	};
 };
 
@@ -117,25 +116,331 @@ var OrderViewHandler = function(transaction){
  * model
  ********************************************************************************/
 var OrderModel = function (){
-
-	var vo = {
+	
+	var getOrderType01 = function (){
+		return {
+			orderId     : $("#orderId").val(),
+			itemCls     : $("#itemCls").val(),
+			company     : $("#company").val(),
 			
+			itemNm      : $("#itemNm01").val(),
+			itemSpec    : $("#itemSpec01").val(),
+			totalQty    : $("#totalQty01").val(),
+			paper1      : $("#paper101").val(),
+			paper2      : $("#paper201").val(),
+			paper3      : $("#paper301").val(),
+			paper4      : $("#paper401").val(),
+			parerRoll   : $("#parerRoll01").val(),
+			rollQty     : $("#rollQty01").val(),
+			dueDate     : $("#dueDate01").val(),
+			delivery    : $("#delivery01").val(),
+			
+			description : $("#description").val(),
+			file1       : $("#file1").val(),
+			file2       : $("#file2").val(),
+
+			setEnd      : $("#setEnd").val(),
+			endDate     : $("#endDate").val(),
+			endTime     : $("#endTime").val(),
+
+			userId      : $("#userId").val(),
+			newDate     : $("#newDate").val(),
+			newTime     : $("#newTime").val(),
+			modDate     : $("#modDate").val(),
+			modTime	    : $("#modTime").val(),
+		};
+	};
+	var setOrderType01 = function (vo){
+		$("#orderId").val(vo.orderId);
+		$("#itemCls").val(vo.itemCls);
+		$("#company").val(vo.company);
+		
+		$("#itemNm01").val(vo.itemNm);
+		$("#itemSpec01").val(vo.itemSpec);
+		$("#totalQty01").val(vo.totalQty);
+		$("#paper101").val(vo.paper1);
+		$("#paper201").val(vo.paper2);
+		$("#paper301").val(vo.paper3);
+		$("#paper401").val(vo.paper4);
+		$("#parerRoll01").val(vo.parerRoll);
+		$("#rollQty01").val(vo.rollQty);
+		$("#dueDate01").val(vo.dueDate);
+		$("#delivery01").val(vo.delivery);
+		
+		$("#description").val(vo.description);
+		$("#file1").val(vo.file1);
+		$("#file2").val(vo.file2);
+
+		$("#setEnd").val(vo.setEnd);
+		$("#endDate").val(vo.endDate);
+		$("#endTime").val(vo.endTime);
+
+		$("#userId").val(vo.userId);
+		$("#newDate").val(vo.newDate);
+		$("#newTime").val(vo.newTime);
+		$("#modDate").val(vo.modDate);
+		$("#modTime").val(vo.modTime);
 	};
 	
-	var getOrderType01 = function (){};
-	var setOrderType01 = function (){};
+	var getOrderType02 = function (){
+		return {
+			orderId     : $("#orderId").val(),
+			itemCls     : $("#itemCls").val(),
+			company     : $("#company").val(),
+			
+			itemNm      : $("#itemNm02").val(),
+			itemSpec    : $("#itemSpec02").val(),
+			totalQty    : $("#totalQty02").val(),
+			paper1      : $("#paper102").val(),
+			paper2      : $("#paper202").val(),
+			paper3      : $("#paper302").val(),
+			paper4      : $("#paper402").val(),
+			parerRoll   : $("#parerRoll02").val(),
+			rollQty     : $("#rollQty02").val(),
+			dueDate     : $("#dueDate02").val(),
+			delivery    : $("#delivery02").val(),
+			
+			description : $("#description").val(),
+			file1       : $("#file1").val(),
+			file2       : $("#file2").val(),
+
+			setEnd      : $("#setEnd").val(),
+			endDate     : $("#endDate").val(),
+			endTime     : $("#endTime").val(),
+
+			userId      : $("#userId").val(),
+			newDate     : $("#newDate").val(),
+			newTime     : $("#newTime").val(),
+			modDate     : $("#modDate").val(),
+			modTime	    : $("#modTime").val(),
+		};
+	};
+	var setOrderType02 = function (){
+		$("#orderId").val(vo.orderId);
+		$("#itemCls").val(vo.itemCls);
+		$("#company").val(vo.company);
+		
+		$("#itemNm02").val(vo.itemNm);
+		$("#itemSpec02").val(vo.itemSpec);
+		$("#totalQty02").val(vo.totalQty);
+		$("#paper102").val(vo.paper1);
+		$("#paper202").val(vo.paper2);
+		$("#paper302").val(vo.paper3);
+		$("#paper402").val(vo.paper4);
+		$("#parerRoll02").val(vo.parerRoll);
+		$("#rollQty02").val(vo.rollQty);
+		$("#dueDate02").val(vo.dueDate);
+		$("#delivery02").val(vo.delivery);
+		
+		$("#description").val(vo.description);
+		$("#file1").val(vo.file1);
+		$("#file2").val(vo.file2);
+
+		$("#setEnd").val(vo.setEnd);
+		$("#endDate").val(vo.endDate);
+		$("#endTime").val(vo.endTime);
+
+		$("#userId").val(vo.userId);
+		$("#newDate").val(vo.newDate);
+		$("#newTime").val(vo.newTime);
+		$("#modDate").val(vo.modDate);
+		$("#modTime").val(vo.modTime);
+	};
 	
-	var getOrderType02 = function (){};
-	var setOrderType02 = function (){};
+	var getOrderType03 = function (){
+		return {
+			orderId     : $("#orderId").val(),
+			itemCls     : $("#itemCls").val(),
+			company     : $("#company").val(),
+			
+			itemNm      : $("#itemNm03").val(),
+			itemSpec    : $("#itemSpec03").val(),
+			totalQty    : $("#totalQty03").val(),
+			paper1      : $("#paper103").val(),
+			paper2      : $("#paper203").val(),
+			paper3      : $("#paper303").val(),
+			paper4      : $("#paper403").val(),
+			parerRoll   : $("#parerRoll03").val(),
+			rollQty     : $("#rollQty03").val(),
+			dueDate     : $("#dueDate03").val(),
+			delivery    : $("#delivery03").val(),
+			
+			description : $("#description").val(),
+			file1       : $("#file1").val(),
+			file2       : $("#file2").val(),
+
+			setEnd      : $("#setEnd").val(),
+			endDate     : $("#endDate").val(),
+			endTime     : $("#endTime").val(),
+
+			userId      : $("#userId").val(),
+			newDate     : $("#newDate").val(),
+			newTime     : $("#newTime").val(),
+			modDate     : $("#modDate").val(),
+			modTime	    : $("#modTime").val(),
+		};
+	};
+	var setOrderType03 = function (){
+		$("#orderId").val(vo.orderId);
+		$("#itemCls").val(vo.itemCls);
+		$("#company").val(vo.company);
+		
+		$("#itemNm03").val(vo.itemNm);
+		$("#itemSpec03").val(vo.itemSpec);
+		$("#totalQty03").val(vo.totalQty);
+		$("#paper103").val(vo.paper1);
+		$("#paper203").val(vo.paper2);
+		$("#paper303").val(vo.paper3);
+		$("#paper403").val(vo.paper4);
+		$("#parerRoll03").val(vo.parerRoll);
+		$("#rollQty03").val(vo.rollQty);
+		$("#dueDate03").val(vo.dueDate);
+		$("#delivery03").val(vo.delivery);
+		
+		$("#description").val(vo.description);
+		$("#file1").val(vo.file1);
+		$("#file2").val(vo.file2);
+
+		$("#setEnd").val(vo.setEnd);
+		$("#endDate").val(vo.endDate);
+		$("#endTime").val(vo.endTime);
+
+		$("#userId").val(vo.userId);
+		$("#newDate").val(vo.newDate);
+		$("#newTime").val(vo.newTime);
+		$("#modDate").val(vo.modDate);
+		$("#modTime").val(vo.modTime);
+	};
 	
-	var getOrderType03 = function (){};
-	var setOrderType03 = function (){};
+	var getOrderType04 = function (){
+		return {
+			orderId     : $("#orderId").val(),
+			itemCls     : $("#itemCls").val(),
+			company     : $("#company").val(),
+			
+			itemNm      : $("#itemNm04").val(),
+			itemSpec    : $("#itemSpec04").val(),
+			totalQty    : $("#totalQty04").val(),
+			paper1      : $("#paper104").val(),
+			paper2      : $("#paper204").val(),
+			paper3      : $("#paper304").val(),
+			paper4      : $("#paper404").val(),
+			parerRoll   : $("#parerRoll04").val(),
+			rollQty     : $("#rollQty04").val(),
+			dueDate     : $("#dueDate04").val(),
+			delivery    : $("#delivery04").val(),
+			
+			description : $("#description").val(),
+			file1       : $("#file1").val(),
+			file2       : $("#file2").val(),
+
+			setEnd      : $("#setEnd").val(),
+			endDate     : $("#endDate").val(),
+			endTime     : $("#endTime").val(),
+
+			userId      : $("#userId").val(),
+			newDate     : $("#newDate").val(),
+			newTime     : $("#newTime").val(),
+			modDate     : $("#modDate").val(),
+			modTime	    : $("#modTime").val(),
+		};
+	};
+	var setOrderType04 = function (){
+		$("#orderId").val(vo.orderId);
+		$("#itemCls").val(vo.itemCls);
+		$("#company").val(vo.company);
+		
+		$("#itemNm04").val(vo.itemNm);
+		$("#itemSpec04").val(vo.itemSpec);
+		$("#totalQty04").val(vo.totalQty);
+		$("#paper104").val(vo.paper1);
+		$("#paper204").val(vo.paper2);
+		$("#paper304").val(vo.paper3);
+		$("#paper404").val(vo.paper4);
+		$("#parerRoll04").val(vo.parerRoll);
+		$("#rollQty04").val(vo.rollQty);
+		$("#dueDate04").val(vo.dueDate);
+		$("#delivery04").val(vo.delivery);
+		
+		$("#description").val(vo.description);
+		$("#file1").val(vo.file1);
+		$("#file2").val(vo.file2);
+
+		$("#setEnd").val(vo.setEnd);
+		$("#endDate").val(vo.endDate);
+		$("#endTime").val(vo.endTime);
+
+		$("#userId").val(vo.userId);
+		$("#newDate").val(vo.newDate);
+		$("#newTime").val(vo.newTime);
+		$("#modDate").val(vo.modDate);
+		$("#modTime").val(vo.modTime);
+	};
 	
-	var getOrderType04 = function (){};
-	var setOrderType04 = function (){};
-	
-	var getOrderType05 = function (){};
-	var setOrderType05 = function (){};
+	var getOrderType05 = function (){
+		return {
+			orderId     : $("#orderId").val(),
+			itemCls     : $("#itemCls").val(),
+			company     : $("#company").val(),
+			
+			itemNm      : $("#itemNm05").val(),
+			itemSpec    : $("#itemSpec05").val(),
+			totalQty    : $("#totalQty05").val(),
+			paper1      : $("#paper105").val(),
+			paper2      : $("#paper205").val(),
+			paper3      : $("#paper305").val(),
+			paper4      : $("#paper405").val(),
+			parerRoll   : $("#parerRoll05").val(),
+			rollQty     : $("#rollQty05").val(),
+			dueDate     : $("#dueDate05").val(),
+			delivery    : $("#delivery05").val(),
+			
+			description : $("#description").val(),
+			file1       : $("#file1").val(),
+			file2       : $("#file2").val(),
+
+			setEnd      : $("#setEnd").val(),
+			endDate     : $("#endDate").val(),
+			endTime     : $("#endTime").val(),
+
+			userId      : $("#userId").val(),
+			newDate     : $("#newDate").val(),
+			newTime     : $("#newTime").val(),
+			modDate     : $("#modDate").val(),
+			modTime	    : $("#modTime").val(),
+		};
+	};
+	var setOrderType05 = function (){
+		$("#orderId").val(vo.orderId);
+		$("#itemCls").val(vo.itemCls);
+		$("#company").val(vo.company);
+		
+		$("#itemNm05").val(vo.itemNm);
+		$("#itemSpec05").val(vo.itemSpec);
+		$("#totalQty05").val(vo.totalQty);
+		$("#paper105").val(vo.paper1);
+		$("#paper205").val(vo.paper2);
+		$("#paper305").val(vo.paper3);
+		$("#paper405").val(vo.paper4);
+		$("#parerRoll05").val(vo.parerRoll);
+		$("#rollQty05").val(vo.rollQty);
+		$("#dueDate05").val(vo.dueDate);
+		$("#delivery05").val(vo.delivery);
+		
+		$("#description").val(vo.description);
+		$("#file1").val(vo.file1);
+		$("#file2").val(vo.file2);
+
+		$("#setEnd").val(vo.setEnd);
+		$("#endDate").val(vo.endDate);
+		$("#endTime").val(vo.endTime);
+
+		$("#userId").val(vo.userId);
+		$("#newDate").val(vo.newDate);
+		$("#newTime").val(vo.newTime);
+		$("#modDate").val(vo.modDate);
+		$("#modTime").val(vo.modTime);
+	};
 	
 	return {
 		
